6619af434662514e006b9ec73dba9dc00bf9afec
oet <dhdmsxor1@cj.net>
2024-08-18 21:54:45 +0900

    useMemo 적용

--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import "./List.css";
 import TodoItem from './TodoItem';
 const List = ({todos, onUpdate, onDelete}) => {
@@ -19,10 +19,45 @@ const List = ({todos, onUpdate, onDelete}) => {
 
   const filteredTodos = getFilteredData();
 
+  // const getAnalyzedData = () => {
+  //   const totalCount = todos.length;
+  //   const doneCount = todos.filter(
+  //     (todo) => todo.isDone
+  //   ).length;
+  //   const notDoneCount = totalCount - doneCount;
+
+  //   return {
+  //     totalCount,
+  //     doneCount,
+  //     notDoneCount
+  //   }
+  // };
+
+  const {totalCount,doneCount, notDoneCount} = useMemo(() => {
+    console.log("ascs" + todos);
+    const totalCount = todos.length;
+    const doneCount = todos.filter(
+      (todo) => todo.isDone
+    ).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount
+    }
+  }, [todos])
+
+  // const {totalCount, doneCount, notDoneCount} = getAnalyzedData()
 
   return (
     <div className="List">
       <h4>Todo List 🌱</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>doneCount: {doneCount}</div>
+        <div>notDoneCount: {notDoneCount}</div>
+      </div>
       <input type="text" value={search} onChange={onChangeSearch} placeholder='검색어를 입력하세요'/>
       <div className='todos_wrapper'>
         {filteredTodos.map((todo)=> {
