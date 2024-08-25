e4fba5b2c40f5ed26b231dd1b247d24b6042a77c
oet <dhdmsxor1@cj.net>
2024-08-25 21:52:00 +0900

    Home 레이아웃 작업

--- section11/src/pages/Home.jsx
+++ section11/src/pages/Home.jsx
@@ -1,5 +1,16 @@
+import Button from '../components/Button';
+import DiaryList from '../components/DiaryList';
+import Header from '../components/Header';
+
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      <Header title={"2024년 8월"} 
+      leftChild={<Button text={"<"}></Button>} 
+      rightChild={<Button text={">"}></Button>}></Header>
+      <DiaryList></DiaryList>
+    </div>
+  )
 };
 
 export default Home;
